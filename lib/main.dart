956182f2a0d53ecca8d3ba72de83f68b6acc16a6
Muhaiminul Islam Hasin <mdmuhaiminul5430@gmail.com>
2025-10-23 01:11:07 +0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:doto_manager/app.dart';
+
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(TaskManagerApp());
+}
+
+
---
+++ lib/ui/screens/new_task_screen.dart
@@ -0,0 +1,135 @@
+import 'package:doto_manager/Data/model/task_model.dart';
+import 'package:doto_manager/Data/model/task_status_count.dart';
+import 'package:doto_manager/Data/services/api_caller.dart';
+import 'package:doto_manager/Data/utils/urls.dart';
+import 'package:doto_manager/ui/screens/add_new_task_screen.dart';
+import 'package:doto_manager/ui/widgets/centered_progress_indecator.dart';
+import 'package:doto_manager/ui/widgets/snak_bar_message.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/task_card.dart';
+import '../widgets/task_count_by_status_card.dart';
+
+class NewTaskScreen extends StatefulWidget {
+  const NewTaskScreen({super.key});
+
+  @override
+  State<NewTaskScreen> createState() => _NewTaskScreenState();
+}
+
+class _NewTaskScreenState extends State<NewTaskScreen> {
+  bool _getTaskStatusCountInProgress = false;
+  bool _getNewTaskInProgress = false;
+  List<TaskStatusCountModel> _taskStatusCountList = [];
+  List<TaskModel> _newTaskList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getAllTaskStatusCount();
+    _getAllNewTasks();
+  }
+
+  Future<void> _getAllTaskStatusCount() async {
+    _getTaskStatusCountInProgress = true;
+    setState(() {});
+    final ApiResponse response = await ApiCaller.getRequest(
+      url: Urls.taskStatusCountUrl,
+    );
+    if (response.isSuccess) {
+      List<TaskStatusCountModel> list = [];
+      for (Map<String, dynamic> jsonData in response.responseData['data']) {
+        list.add(TaskStatusCountModel.fromJson(jsonData));
+      }
+      _taskStatusCountList = list;
+    } else {
+      showSnackBarMessage(context, response.errorMessage!);
+    }
+    _getTaskStatusCountInProgress = false;
+    setState(() {});
+  }
+
+  Future<void> _getAllNewTasks() async {
+    _getNewTaskInProgress = true;
+    setState(() {});
+    final ApiResponse response = await ApiCaller.getRequest(
+      url: Urls.newTaskListUrl,
+    );
+    if (response.isSuccess) {
+      List<TaskModel> list = [];
+      for (Map<String, dynamic> jsonData in response.responseData['data']) {
+        list.add(TaskModel.fromJson(jsonData));
+      }
+      _newTaskList = list;
+    } else {
+      showSnackBarMessage(context, response.errorMessage!);
+    }
+    _getNewTaskInProgress = false;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            SizedBox(
+              height: 90,
+              child: Visibility(
+                visible: _getTaskStatusCountInProgress == false,
+                replacement: CenteredProgressIndecator(),
+                child: ListView.separated(
+                  itemCount: _taskStatusCountList.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return TaskCountByStatusCard(
+                      title: _taskStatusCountList[index].status,
+                      count: _taskStatusCountList[index].count,
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(width: 4);
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: Visibility(
+                visible: _getNewTaskInProgress == false,
+                replacement: CenteredProgressIndecator(),
+                child: ListView.separated(
+                  itemCount: _newTaskList.length,
+                  itemBuilder: (context, index) {
+                    return TaskCard(
+                      taskModel: _newTaskList[index],
+                      refreshParent: () {
+                        _getAllNewTasks();
+                      },
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(height: 8);
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onTapAddNewTaskButton,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _onTapAddNewTaskButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const AddNewTaskScreen()),
+    );
+  }
+}
